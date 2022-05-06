a19873eb1fb3f5231b03a8522f70dd1ff2c537de
antoniolpcan <antoniolpctwice@gmail.com>
2022-05-06 12:41:52 -0300

    change router

--- routers/backup.py
+++ routers/backup.py
@@ -1,12 +1,8 @@
 from vars import router_names
 from .router_config import router_imap
 from BackupMail.Backup import BackupMail
-#from fastapi.responses import FileResponse
 
 @router_imap.post(router_names[1])
 async def mail_backup(bup: BackupMail) -> dict:
     response = bup.get_mail()
-    #name = response.get('Name_file')
-    print(response)
     return response
-    #return FileResponse(path = f'{ziped_dir}{name}',filename = f'{name}',media_type=f"{zip_format}")
--- routers/login.py
+++ routers/login.py
@@ -1,5 +1,5 @@
-from .router_config import router_imap
 from vars import router_names
+from .router_config import router_imap
 from BackupMail.Backup import BackupMail
 
 @router_imap.post(router_names[0])
